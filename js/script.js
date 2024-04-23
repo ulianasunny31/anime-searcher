cbb4e2425f2ee8f12437d8e56a07c9b1dac1d0bf
Uliana Seredenko <uliana1331@gmail.com>
2024-04-23 15:13:53 +0300

    big card start

--- js/script.js
+++ js/script.js
@@ -1,18 +1,26 @@
-const input = document.querySelector(".search-inp");
-const searchBtn = document.querySelector(".search-btn");
-const mainContainer = document.querySelector(".main-container");
-const loadMoreBtn = document.querySelector("#load-more-btn");
-let timerID, inputValue;
-let page = 1;
-let perPage = 24;
+import {
+  timerID,
+  inputValue,
+  input,
+  searchBtn,
+  mainContainer,
+  loadMoreBtn,
+  page,
+  perPage,
+  modalBackdrop,
+} from "../refs.js";
+
 const params = new URLSearchParams({
   limit: perPage,
   page: page,
 });
 
 let url = `https://api.jikan.moe/v4/anime?${params}`;
-// const { title, synopsis, year, rating, genres, studios, image_url } = anime;
 
+//Показывает все карточки изначально на странице
+animeFetcher(drawSmallAnimeCard, url);
+
+//Вызов на сервер для получения инф про аниме, вкладывается ф-я по структуре карточек и ссылка на вызов как параметры
 function animeFetcher(func, link) {
   fetch(link)
     .then((response) => response.json())
@@ -22,6 +30,7 @@ function animeFetcher(func, link) {
     .catch((error) => console.log("Error:", error));
 }
 
+//Получает результат вызова на сервер(animeData) и имеет в себе перебор. а потом создание карточек аниме(foo)
 function createCards(animeData, foo) {
   if (animeData.length === 0) {
     mainContainer.innerHTML = `<h2 class="nothing-heading">Nothing was found</h2>`;
@@ -38,13 +47,13 @@ function createCards(animeData, foo) {
   }
 }
 
-animeFetcher(drawSmallAnimeCard, url);
-
+//Загружает больше карточек аниме
 loadMoreBtn.addEventListener("click", (e) => {
   e.preventDefault();
   animeFetcher(drawSmallAnimeCard, url);
 });
 
+//Создает структуру маленьких карточек аниме
 function drawSmallAnimeCard(anime) {
   const { title, images } = anime;
   const smallImageUrl = images.jpg.large_image_url;
@@ -58,6 +67,7 @@ function drawSmallAnimeCard(anime) {
   mainContainer.insertAdjacentHTML("beforeend", smallAnimeCard);
 }
 
+//Совершает поиск по вводу имени аниме
 searchBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
@@ -72,3 +82,25 @@ searchBtn.addEventListener("click", (e) => {
     }
   }, 1000);
 });
+
+function drawBigCard(anime) {
+  const { title, synopsis, year, rating, genres, studios, image_url } = anime;
+  const bigImageUrl = images.jpg.large_image_url;
+
+  let bigAnimeCard = `
+  <div class="modal">
+  <img class="anime-image-big" src="${bigImageUrl}" alt="${title}" width="" height=""/>
+  <div>
+  <h3 class="title">Title: ${title}</h3>
+  <h3 class="rating">Rating: ${rating}</h3>
+  <h3 class="year">Year: ${year}</h3>
+  <h3 class="studios">Studios: ${studios}</h3>
+  <h3 class="genre">Genre: ${genre}</h3>
+  </div>
+  <p class="synopsis">${synopsis}</p>
+
+  </div>
+  `;
+
+  modalBackdrop.insertAdjacentHTML("beforeend", bigAnimeCard);
+}
