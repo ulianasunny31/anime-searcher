2dfaf21a547b1ebf9105115c498a6bad2df867ec
Uliana Seredenko <uliana1331@gmail.com>
2024-05-08 17:36:38 +0300

    small card html icon

--- src/js/script.js
+++ src/js/script.js
@@ -66,8 +66,11 @@ function drawSmallCard(anime) {
 
   let smallAnimeCard = `
   <div class="anime-card" data-anime-id="${mal_id}">
-  <img class="anime-image-small" src="${smallImageUrl}" alt="${title}" />
-  <h3 class="anime-small-heading">${realTitle}</h3>
+   <img class="anime-image-small" src="${smallImageUrl}" alt="${title}" />
+   <h3 class="anime-small-heading">${realTitle}</h3>
+   <svg id="add-icon" class="not-visible" width="" height="">
+    <use href="./images/symbol-defs.svg#icon-add"></use>
+   </svg>
   </div>
   `;
   mainContainer.insertAdjacentHTML('beforeend', smallAnimeCard);
